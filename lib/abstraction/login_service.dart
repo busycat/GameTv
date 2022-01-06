616c7fbb8adc0c7591883afdd321401bb03d7246
RedHalo <87261796+busycat@users.noreply.github.com>
2022-01-07 05:10:28 +0530

    Add Image, User name /Password Field

--- lib/abstraction/login_service.dart
+++ lib/abstraction/login_service.dart
@@ -6,15 +6,17 @@ abstract class LoginService {
   Future<void> logout(String token);
 }
 
+/// Should be private, But intentionally made public to reuse later
+final Map<String, String> credentials = {
+  '9898989898': 'password123',
+  '9876543210': 'password123',
+};
+final Map<String, User> _users = {
+  '9898989898': const User(name: 'Simon Baker', age: 25, token: '1234'),
+  '9876543210': const User(name: 'Dikki  Hanger', age: 22, token: '1234'),
+};
+
 class MockLoginService extends LoginService {
-  static final Map<String, String> credentials = {
-    '9898989898': 'password123',
-    '9876543210': 'password123',
-  };
-  static final Map<String, User> users = {
-    '9898989898': const User(name: 'Simon Baker', age: 25, token: '1234'),
-    '9876543210': const User(name: 'Dikki  Hanger', age: 22, token: '1234'),
-  };
   @override
   Future<User> login(String user, String password) async {
     await Future<void>.delayed(Duration(seconds: 1));
@@ -22,7 +24,7 @@ class MockLoginService extends LoginService {
     if (credentials.containsKey(user)) {
       if (credentials[user] == password) {
         /// Now we know User is a Valid user with Correct Password
-        final userObj = users[user];
+        final userObj = _users[user];
         if (userObj != null) {
           return userObj;
         } else {
--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,32 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:game_tv/abstraction/login_service.dart';
+import 'package:game_tv/pages/home/home_page.dart';
 import 'package:game_tv/provider/user_service_provider.dart';
 import 'package:provider/provider.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   static const route = '/login';
 
   const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _usernameController =
+          TextEditingController(text: credentials.keys.first),
+      _passwordController =
+          TextEditingController(text: credentials.values.first);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Consumer<UserService>(builder: (context, userService, child) {
+      final mq = MediaQuery.of(context).size.width;
+
       onLogin() async {
-        await userService.login('123', '456');
+        await userService.login(
+          _usernameController.text,
+          _passwordController.text,
+        );
         if (userService.isAuthenticated) {
-          Navigator.of(context).pushNamed(Login.route);
+          Navigator.of(context).popAndPushNamed(HomePage.route);
         }
       }
 
       return SafeArea(
-        child: Column(
-          children: [
-            Text('Login Page'),
-            Text('Error : ' + (userService.error ?? 'None')),
-            Text(userService.isAuthenticated ? 'LoggedIn' : 'Not Loggedin'),
-            TextButton(onPressed: onLogin, child: Text('Login'))
-          ],
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0x00000000),
+                ),
+                child: Image(
+                  image: AssetImage('assets/images/game.tv-logo.png'),
+                  width: 0.5 * mq,
+                ),
+              ),
+              Text('Login Page'),
+              TextField(
+                controller: _usernameController,
+              ),
+              TextField(
+                controller: _passwordController,
+              ),
+              Text('Error : ' + (userService.error ?? 'None')),
+              Text(userService.isAuthenticated ? 'LoggedIn' : 'Not Loggedin'),
+              TextButton(onPressed: onLogin, child: Text('Login'))
+            ],
+          ),
         ),
       );
     }));
