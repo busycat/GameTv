131d9675a94b18b6b79c46a74316610f5b7c3f0d
RedHalo <87261796+busycat@users.noreply.github.com>
2022-01-07 03:12:50 +0530

    Added Initial Page

---
+++ lib/pages/index.dart
@@ -0,0 +1,1 @@
+export './login/login-page.dart';
---
+++ lib/pages/login/login-page.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello World');
+  }
+}
