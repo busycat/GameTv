574a7e5d9363e437cdda396027411fa7b410ca8f
RedHalo <87261796+busycat@users.noreply.github.com>
2022-01-07 03:53:10 +0530

    Add Logout and Login Functions

--- lib/abstraction/login_service.dart
+++ lib/abstraction/login_service.dart
@@ -3,15 +3,15 @@ import 'package:game_tv/models/user.dart';
 
 abstract class LoginService {
   Future<User> login(String user, String password);
-  Future<User> logout(String user, String password);
+  Future<void> logout(String token);
 }
 
 class MockLoginService extends LoginService {
-  final Map<String, String> credentials = {
+  static final Map<String, String> credentials = {
     '9898989898': 'password123',
     '9876543210': 'password123',
   };
-  final Map<String, User> users = {
+  static final Map<String, User> users = {
     '9898989898': const User(name: 'Simon Baker', age: 25, token: '1234'),
     '9876543210': const User(name: 'Dikki  Hanger', age: 22, token: '1234'),
   };
@@ -34,8 +34,6 @@ class MockLoginService extends LoginService {
   }
 
   @override
-  Future<User> logout(String user, String password) {
-    // TODO: implement logout
-    throw UnimplementedError();
-  }
+  Future<void> logout(String token) =>
+      Future<void>.delayed(Duration(seconds: 1));
 }
--- lib/provider/user_service_provider.dart
+++ lib/provider/user_service_provider.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:game_tv/abstraction/app_exception.dart';
+import 'package:game_tv/abstraction/login_service.dart';
 import 'package:game_tv/models/user.dart';
 
 class UserService with ChangeNotifier {
+  static final LoginService _loginService = MockLoginService();
   User? _user;
+  Future<void> login(String user, String password) async {
+    final userObj = await _loginService.login(user, password);
 
-  void login() {}
+    /// TODO Save Info to Local/Persisting Storage
+    _user = userObj;
+  }
+
+  Future<void> logout() async {
+    final token = _user?.token;
+    if (token == null) {
+      throw AppException(error: "User Token doesn't exist", code: 500);
+    }
+    await _loginService.logout(token);
+  }
 }
