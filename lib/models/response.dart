104c795515b4b790fb40cdcad3260a2ab90a986e
RedHalo <87261796+busycat@users.noreply.github.com>
2022-01-07 05:42:41 +0530

    GN

--- lib/models/response.dart
+++ lib/models/response.dart
@@ -1,79 +1,50 @@
-class Response {
-  int code;
-  Data data;
-  bool success;
+import 'dart:convert';
 
-  Response({
+RecommendationDto recommendationDtoFromJson(String str) =>
+    RecommendationDto.fromJson(json.decode(str));
+
+class RecommendationDto {
+  RecommendationDto({
     required this.code,
     required this.data,
     required this.success,
   });
+
+  int code;
+  Data data;
+  bool success;
+
+  factory RecommendationDto.fromJson(Map<String, dynamic> json) =>
+      RecommendationDto(
+        code: json['code'] as int,
+        data: Data.fromJson(json['data']),
+        success: json['success'],
+      );
 }
 
 class Data {
-  String cursor;
-  dynamic tournamentCount;
-  List<Tournament> tournaments;
-  bool isLastBatch;
-
   Data({
     required this.cursor,
     required this.tournamentCount,
     required this.tournaments,
     required this.isLastBatch,
   });
+
+  String cursor;
+  dynamic tournamentCount;
+  List<Tournament> tournaments;
+  bool isLastBatch;
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        cursor: json['cursor'] as String,
+        tournamentCount: json['tournament_count'],
+        tournaments: List<Tournament>.from(
+            json['tournaments'].map((x) => Tournament.fromJson(x))),
+        isLastBatch: json['is_last_batch'],
+      );
 }
 
 class Tournament {
-  bool isCheckInRequired;
-  String tournamentId;
-  bool tournamentEnded;
-  String tournamentEndDate;
-  bool areRandomTeamsAllowed;
-  int registeredTeams;
-  String adminLocale;
-  String regEndDate;
-  String startDate;
-  String rules;
-  int maxTeams;
-  String tournamentUrl;
-  String prizes;
-  String matchStyleType;
-  String pwaUrl;
-  TournamentType tournamentType;
-  String geo;
-  String maxLevelId;
-  bool isPasswordRequired;
-  String name;
-  MatchStyle matchStyle;
-  String registrationUrl;
-  dynamic gamePkg;
-  bool isRegistrationOpen;
-  bool isWaitlistEnabled;
-  bool incompleteTeamsAllowed;
-  bool isAutoResultAllowed;
-  int teamSize;
-  Status status;
-  bool isLevelsEnabled;
-  bool indexPage;
-  String dynamicAppUrl;
-  String minLevelId;
-  String gameFormat;
-  String details;
-  String gameIconUrl;
-  String regStartDate;
-  String coverUrl;
-  dynamic bracketsUrl;
-  String tournamentSlug;
-  String discordUrl;
-  String gameId;
-  bool resultSubmissionByAdmin;
-  String country;
-  String adminUsername;
-  String gameName;
-  String streamUrl;
-  int winnersCount;
-
   Tournament({
     required this.isCheckInRequired,
     required this.tournamentId,
@@ -124,10 +95,135 @@ class Tournament {
     required this.streamUrl,
     required this.winnersCount,
   });
+
+  bool isCheckInRequired;
+  String tournamentId;
+  bool tournamentEnded;
+  String tournamentEndDate;
+  bool areRandomTeamsAllowed;
+  int registeredTeams;
+  String adminLocale;
+  String regEndDate;
+  String startDate;
+  String rules;
+  int maxTeams;
+  String tournamentUrl;
+  String prizes;
+  String matchStyleType;
+  String pwaUrl;
+  TournamentType tournamentType;
+  String geo;
+  String maxLevelId;
+  bool isPasswordRequired;
+  String name;
+  MatchStyle matchStyle;
+  String registrationUrl;
+  dynamic gamePkg;
+  bool isRegistrationOpen;
+  bool isWaitlistEnabled;
+  bool incompleteTeamsAllowed;
+  bool isAutoResultAllowed;
+  int teamSize;
+  Status status;
+  bool isLevelsEnabled;
+  bool indexPage;
+  String dynamicAppUrl;
+  String minLevelId;
+  String gameFormat;
+  String details;
+  String gameIconUrl;
+  String regStartDate;
+  String coverUrl;
+  dynamic bracketsUrl;
+  String tournamentSlug;
+  String discordUrl;
+  String gameId;
+  bool resultSubmissionByAdmin;
+  String country;
+  String adminUsername;
+  String gameName;
+  String streamUrl;
+  int winnersCount;
+
+  factory Tournament.fromJson(Map<String, dynamic> json) => Tournament(
+        isCheckInRequired: json['is_check_in_required'],
+        tournamentId: json['tournament_id'],
+        tournamentEnded: json['tournament_ended'],
+        tournamentEndDate: json['tournament_end_date'],
+        areRandomTeamsAllowed: json['are_random_teams_allowed'],
+        registeredTeams:
+            json['registered_teams'] == null ? null : json['registered_teams'],
+        adminLocale: json['admin_locale'],
+        regEndDate: json['reg_end_date'],
+        startDate: json['start_date'],
+        rules: json['rules'],
+        maxTeams: json['max_teams'],
+        tournamentUrl: json['tournament_url'],
+        prizes: json['prizes'],
+        matchStyleType: json['match_style_type'],
+        pwaUrl: json['pwa_url'] == null ? null : json['pwa_url'],
+        tournamentType: tournamentTypeValues.map[json['tournament_type']],
+        geo: json['geo'],
+        maxLevelId: json['max_level_id'] == null ? null : json['max_level_id'],
+        isPasswordRequired: json['is_password_required'],
+        name: json['name'],
+        matchStyle: matchStyleValues.map[json['match_style']],
+        registrationUrl: json['registration_url'],
+        gamePkg: json['game_pkg'],
+        isRegistrationOpen: json['is_registration_open'],
+        isWaitlistEnabled: json['is_waitlist_enabled'],
+        incompleteTeamsAllowed: json['incomplete_teams_allowed'],
+        isAutoResultAllowed: json['is_auto_result_allowed'],
+        teamSize: json['team_size'],
+        status: statusValues.map[json['status']],
+        isLevelsEnabled: json['is_levels_enabled'],
+        indexPage: json['index_page'],
+        dynamicAppUrl: json['dynamic_app_url'],
+        minLevelId: json['min_level_id'] == null ? null : json['min_level_id'],
+        gameFormat: json['game_format'],
+        details: json['details'],
+        gameIconUrl: json['game_icon_url'],
+        regStartDate: json['reg_start_date'],
+        coverUrl: json['cover_url'],
+        bracketsUrl: json['brackets_url'],
+        tournamentSlug: json['tournament_slug'],
+        discordUrl: json['discord_url'],
+        gameId: json['game_id'],
+        resultSubmissionByAdmin: json['result_submission_by_admin'],
+        country: json['country'],
+        adminUsername: json['admin_username'],
+        gameName: json['game_name'],
+        streamUrl: json['stream_url'],
+        winnersCount:
+            json['winners_count'] == null ? null : json['winners_count'],
+      );
 }
 
 enum MatchStyle { LOBBY, BRACKETS }
 
+final matchStyleValues =
+    EnumValues({'brackets': MatchStyle.BRACKETS, 'lobby': MatchStyle.LOBBY});
+
 enum Status { PUBLISHED }
 
+final statusValues = EnumValues({'published': Status.PUBLISHED});
+
 enum TournamentType { DISCORD, WEB, APP }
+
+final tournamentTypeValues = EnumValues({
+  'app': TournamentType.APP,
+  'discord': TournamentType.DISCORD,
+  'web': TournamentType.WEB
+});
+
+class EnumValues<T> {
+  final Map<String, T> map;
+  late Map<T, String>? _reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    _reverseMap ??= map.map((k, v) => MapEntry(v, k));
+    return _reverseMap!;
+  }
+}
